6fbfa78dfd19e4edfe74f73da314b08f0aad12d5
Alando Appling <alandoappling@gmail.com>
2019-03-15 21:07:42 -0700

    End points have been added

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true // required for knex
+}
+
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +18,81 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+/// POST
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [id] = await db('zoos').insert(req.body);
+
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo);
+  } catch (error) {
+    const message = 'We ran into an error';
+    res.status(500).json({ message, error });
+  }
+});
+
+/// GET
+server.get('/api/zoos', async (req,res) => {
+  try{
+    const zoos = await db('zoos')
+    res.status(200).json(zoos)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+})
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+/// UPDATE
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+/// DELETE
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
